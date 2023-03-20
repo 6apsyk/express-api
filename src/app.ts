14ca7a411bed2383bbe71ec5a4f28e5b0376cd37
IliaKuryndin <6apsyk@mail.ru>
2023-03-20 23:22:27 +0300

    add Exeption.filter and errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Express} from 'express'
 import {Server} from 'node:http'
+import { ExeptionFilter } from './errors/exeption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UsersController } from './users/users.controller';
 
@@ -8,22 +9,29 @@ export class App {
     server: Server;
     app: Express;
     port: number;
-    logger: LoggerService
-    userController: UsersController
+    logger: LoggerService;
+    userController: UsersController;
+    exeptionFilter: ExeptionFilter;
 
-    constructor(logger: LoggerService, userController: UsersController){
+    constructor(logger: LoggerService, userController: UsersController, exeptionFilter: ExeptionFilter){
         this.port = 8000
         this.app = express()
         this.logger = logger
         this.userController = userController
+        this.exeptionFilter = exeptionFilter
     }
 
     useRoutes(){
-        this.app.use('users', this.userController.router)
+        this.app.use('/users', this.userController.router)
+    }
+
+    useExeptionFilter(){
+        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
     }
 
     async init(){
         this.useRoutes()
+        this.useExeptionFilter()
         this.server = this.app.listen(this.port)
         this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
     }
