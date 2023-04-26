b3286cde955baf6e425808912994f0daf503cbce
IliaKuryndin <6apsyk@mail.ru>
2023-04-26 15:03:00 +0300

    add ContainerModule DI

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,9 +5,10 @@ import {injectable, inject} from 'inversify'
 import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.service.interface';
 import 'reflect-metadata';
+import { IUsersController } from './users.controller.interface';
 
 @injectable()
-export class UsersController extends BaseController{
+export class UsersController extends BaseController implements IUsersController{
     constructor(@inject(TYPES.ILogger) private loggerService: ILogger){
       super(loggerService)  
       this.bindRoutes([
